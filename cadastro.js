336fbe74dac3a4ddcb225120537e423c98dcb46d
Keven - cc23143 <142425079+cc23143@users.noreply.github.com>
2023-11-10 10:16:31 -0300

    Switch_ajax_axios

--- cadastro.js
+++ cadastro.js
@@ -2,21 +2,8 @@ let Email = document.getElementById('#Email')
 let Senha = document.querySelector('#Senha')
 let func  = '-1'
 
-const VerifCadAPI = function(){
-    const XML = new XMLHttpRequest()
-    XML.onreadystatechange = function(){
-        if(XML.readyState == 4 && XML.status == 200){
-            func = XML.responseText
-        }else if(XML.status == 404){
-            func = "Error 404:page not found."
-        }
-    }
-    XML.open("get","https://localhost:7698/verifCadastro",true)
-    XML.send("?email=" + Email + "&senha=" + Senha)
-    console.log(func + "\n" + XML.readyState + "\n" + XML.status)
-}
+
 function VerifCadastro(){
-    VerifCadAPI()
     switch(func){
             case "gerente":
                 
