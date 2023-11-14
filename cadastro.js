79f15756fd3e8b83e8b69ca703d9fc73a3449f74
Keven - cc23143 <142425079+cc23143@users.noreply.github.com>
2023-11-14 10:25:36 -0300

    preparing axios

--- cadastro.js
+++ cadastro.js
@@ -3,33 +3,23 @@ let Senha = document.querySelector('#Senha')
 let func  = '-1'
 
 
-function VerifCadastro(){
-    const axios = require('axios');
-
-    axios.get(`http://localhost:7698/verifCadastro`, {params:{
-      email: Email.value,
-      senha: Senha.value
-    }}) //?email=${Email.value}&senha=${Senha.value}
-      .then(function (res) {
-        console.log(res)
-        switch(res){
-            case "gerente":
-                //window.location.replace('InterGerente.html')
-            case "cozinhero":
-
-            case "entregador":
-
-            case "garçom":  
-
-            case "undefined" || "-1" || "nothing" || null:
-                
-            default:
-               //window.location.replace('InterCliente.html')
+async function VerifCadastro(){
+    import axios from 'axios';
+    try {
+      const res = await axios.get(`/http://localhost:7698/verifCadastro?email=${Email.value}&senha=${Senha.value}`);
+      /*switch(res){
+        case "gerente":
+            //window.location.replace('InterGerente.html')
+        case "cozinhero":
+        case "entregador":
+        case "garçom":  
+        case "undefined" || "-1" || "nothing" || null:
+            
+        default:
+           //window.location.replace('InterCliente.html')
+      }*/
+      console.log(res);
+    } catch (error) {
+      console.error(error);
     }
-        console.log(res);
-      })
-      .catch(function (error) {
-        console.log("Não")
-        console.error(error);
-      })
-}
+  }
