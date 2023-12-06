20979b821e21ad8e3d6b30df276c27e58040d27a
Keven - cc23143 <142425079+cc23143@users.noreply.github.com>
2023-12-06 08:15:04 -0300

    Cardapio_Start

--- interfaces/Cardapio.js
+++ interfaces/Cardapio.js
@@ -4,9 +4,19 @@ function addCardapio(){
     axios.get(`http://localhost:7698/getCardapioP`)
       .then(function (res) {
         for(i = 0; i < res.data.length;i++){
-          arrayP[i] = JSON.parse(res.data[i])
+          arrayP[i] = JSON.parse(JSON.stringify(res.data[i]))
         }
         console.log(arrayP)
+      })
+      .catch(function (error) {
+        console.error(error)
+      })
+    axios.get(`http://localhost:7698/getCardapioB`)
+      .then(function (res) {
+        for(i = 0; i < res.data.length;i++){
+          arrayB[i] = JSON.parse(JSON.stringify(res.data[i]))
+        }
+        console.log(arrayB)
       })
       .catch(function (error) {
         console.error(error)
